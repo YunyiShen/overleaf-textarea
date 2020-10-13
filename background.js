48e943fa76e2bb201bb19b512cc7457802f7153a
YunyiShen <yshen99@wisc.edu>
2020-10-13 15:50:01 -0500

    firfox compatibility

--- background.js
+++ background.js
@@ -1,13 +1,11 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'www.overleaf.com'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        if (changeInfo.status === 'complete' && tab.url.match("www.overleaf.com")) {
+            chrome.pageAction.show(tabId);
+        } 
     });
 });
+
+
